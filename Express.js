aea91e827cf5b96f5ce198bb7fbea4f39b694467
Yu Da Lau <yuda@trypencil.com>
2021-10-01 16:29:23 +0800

    update cron

--- Express.js
+++ Express.js
@@ -13,13 +13,13 @@ enableWs(app);
     const channel = await client.createChannel()
     await channel.assertQueue(queue)
 
-    cron.schedule('*/* * * * *', () => {
-        payload = `I am alive at ${new Date()}`
-        channel.sendToQueue(queue, Buffer.from(payload))
-    })
-    
     cron.schedule('*/5 * * * * *', () => {
         payload = 'Every 5 seconds...'
+        channel.sendToQueue(queue, Buffer.from(payload))
+    })
+
+    cron.schedule('*/* * * * *', () => {
+        payload = `I am alive at ${new Date()}`
         channel.sendToQueue(queue, Buffer.from(payload))
     })
 
@@ -29,28 +29,37 @@ enableWs(app);
     })
 
     app.ws('/heartbeat', (ws, req) => {
+        console.log('Client connected');
+
+        const fiveSecondsCronSchedule = cron.schedule('*/5 * * * * *', () => {
+            payload = 'Every 5 seconds...'
+            ws.send(payload)
+        })
+
+        const minuteCronSchedule = cron.schedule('*/* * * * *', () => {
+            payload = `I am alive at ${new Date()}`
+            ws.send(payload)
+        })
+
+        const minute42CronSchedule = cron.schedule('*/42 * * * *', () => {
+            payload = '42 is the meaning to life!'
+            ws.send(payload)
+        })
+
+        // stop cron jobs
+        ws.on('close', () => {
+            console.log("Client disconnecting")
+            fiveSecondsCronSchedule.stop()
+            minuteCronSchedule.stop()
+            minute42CronSchedule.stop()
+        })
+
         // Postman testing
         ws.on('message', msg => {
             console.log(msg)
             ws.send('Received your message')
         })
-
-        cron.schedule('*/* * * * *', () => {
-            payload = `I am alive at ${new Date()}`
-            ws.send(payload)
-        })
-
-        cron.schedule('*/5 * * * * *', () => {
-            payload = 'Every 5 seconds...'
-            ws.send(payload)
-        })
-
-        cron.schedule('*/42 * * * *', () => {
-            payload = '42 is the meaning to life!'
-            ws.send(payload)
-        })
     })
-
 })()
 
 app.listen(3000)
